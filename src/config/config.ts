3dc71d6beb726225bac5b77acc5f5f0962725205
Palanisamy M <palanisamy4034@gmail.com>
2019-09-26 09:30:20 +0530

    Removed the keys

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
 export const twitterConfig = {
-  consumerKey: 'DgHzqXeOlkeGH810guuaPJ9dZ',
-  consumerSecret: 'A2oasuedoncn00wP8oDyUgdNcD19ncr9WsyEP9ibnSWcFwW9za'
+  consumerKey: '',
+  consumerSecret: ''
 };
